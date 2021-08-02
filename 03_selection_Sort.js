74029246f5e722632aee90b42c7b1d510902f08e
Denis Meshcheryakov <53299548+den-mesh@users.noreply.github.com>
2021-08-02 13:20:47 +0400

    Add files via upload

---
+++ 03_selection_Sort.js
@@ -0,0 +1,16 @@
+const selectionSort = arr => {
+    for (let i = 0, l = arr.length, k = l - 1; i < k; i++) {
+        let indexMin = i;
+        for (let j = i + 1; j < l; j++) {
+            if (arr[indexMin] > arr[j]) {
+                indexMin = j;
+            }
+        }
+        if (indexMin !== i) {
+            [arr[i], arr[indexMin]] = [arr[indexMin], arr[i]];
+        }
+    }
+    return arr;
+};
+
+
